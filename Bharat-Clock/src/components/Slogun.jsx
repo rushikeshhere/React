03b5b18133689983346bd5eea2931161d376adff
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-03 18:17:03 +0530

    Written Slogan to show what is the purpose of this App

---
+++ Bharat-Clock/src/components/Slogun.jsx
@@ -0,0 +1,10 @@
+import "../App.css";
+
+function Slogun() {
+  return (
+    <p className="lead">
+      This is the clcok that shows the time in Bharat at all the time{" "}
+    </p>
+  );
+}
+export default Slogun;
