d4c7788bd7c53090cb9b4a238b9389ab1707bb8f
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-03 18:15:13 +0530

    Created ClockTitle component

---
+++ Bharat-Clock/src/components/ClockTitle.jsx
@@ -0,0 +1,10 @@
+import "../App.css";
+
+function ClockTitle() {
+  return (
+    <h1 className="fw-bold" style={{ margin: "10px" }}>
+      Bharat Clock
+    </h1>
+  );
+}
+export default ClockTitle;
