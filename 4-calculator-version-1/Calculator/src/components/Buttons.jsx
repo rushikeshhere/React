45a1d75a56587ca7fb8cbc4c6a7e58cb4fd3956b
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-08 22:06:37 +0530

    used to get input through buttons

---
+++ 4-calculator-version-1/Calculator/src/components/Buttons.jsx
@@ -0,0 +1,32 @@
+import styles from "./Buttons.module.css";
+
+const Buttons = () => {
+  let buttonNames = [
+    "AC",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "9",
+    "0",
+    ".",
+    "=",
+  ];
+
+  return (
+    <div className={styles.buttons}>
+      {buttonNames.map((buttonName) => (
+        <button className={styles.button}>{buttonName}</button>
+      ))}
+    </div>
+  );
+};
+export default Buttons;
