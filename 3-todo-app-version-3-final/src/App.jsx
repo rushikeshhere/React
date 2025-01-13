3c371f7576d152bb48908e7d5ade43c08171af1a
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-13 21:39:13 +0530

    add functionality to add todoItems

---
+++ 3-todo-app-version-3-final/src/App.jsx
@@ -0,0 +1,30 @@
+import AddTodo from "./Components/AddTodo";
+import AppName from "./Components/AppName";
+import TodoItem from "./Components/TodoItem";
+import { useState } from "react";
+
+function App() {
+  // let todoItems = [
+  //   { name: " Buy Milk", dueDate: "2022-01-01" },
+  //   { name: "Go to nanded", dueDate: "2025-01-02" },
+  //   { name: "Birth certificate work ", dueDate: "2025-01-02  " },
+  // ];
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleAddTodo = (itemName, itemDueDate) => {
+    setTodoItems((prev) => [...prev, { name: itemName, dueDate: itemDueDate }]);
+  };
+
+  return (
+    <center>
+      <AppName />
+      <br />
+      <AddTodo addTodoItems={handleAddTodo} />
+      <br />
+      <TodoItem todoItem={todoItems} />
+      <br />
+    </center>
+  );
+}
+
+export default App;
