022cbc98dfe2391687e082f971a229d2c1a610f2
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-13 21:49:29 +0530

    Displayed calculated value

---
+++ 4-calculator-version-2/Calculator-v2/src/components/Display.jsx
@@ -0,0 +1,13 @@
+import styles from "./Display.module.css";
+
+const Display = ({ displayValue }) => {
+  return (
+    <input
+      type="text"
+      className={styles.display}
+      value={displayValue}
+      readOnly
+    ></input>
+  );
+};
+export default Display;
