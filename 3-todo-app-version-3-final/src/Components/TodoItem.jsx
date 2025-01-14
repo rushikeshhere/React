dfb22bcf83734339853e28f12564b585947e7ef3
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-14 22:44:42 +0530

    added event Listener to delete todoItem

--- 3-todo-app-version-3-final/src/Components/TodoItem.jsx
+++ 3-todo-app-version-3-final/src/Components/TodoItem.jsx
@@ -1,11 +1,16 @@
 import TodoItem1 from "./TodoItem1";
 import style from "./TodoItemsContainer.module.css";
 
-const TodoItem = ({ todoItem }) => {
+const TodoItem = ({ todoItem, handleDeleteTodo }) => {
   return (
     <div className={style.container}>
       {todoItem.map((item) => (
-        <TodoItem1 key={item.name} todoName={item.name} date={item.dueDate} />
+        <TodoItem1
+          key={item.name}
+          todoName={item.name}
+          date={item.dueDate}
+          handleDeleteTodo={handleDeleteTodo}
+        />
       ))}
     </div>
   );
