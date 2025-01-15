bf9e9e85487dc08a939cbbbf89ea77599550f51f
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-15 22:49:32 +0530

    added WelcomMessage Component in App.jsx

--- 3-todo-app-version-3-final/src/App.jsx
+++ 3-todo-app-version-3-final/src/App.jsx
@@ -1,6 +1,7 @@
 import AddTodo from "./Components/AddTodo";
 import AppName from "./Components/AppName";
 import TodoItem from "./Components/TodoItem";
+import WelcomeMessage from "./Components/WelcomeMessage";
 import { useState } from "react";
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
       <br />
       <AddTodo addTodoItems={handleAddTodo} />
       <br />
+      <WelcomeMessage todoItem={todoItems}></WelcomeMessage>
       <TodoItem todoItem={todoItems} handleDeleteTodo={handleDeleteTodo} />
       <br />
     </center>
