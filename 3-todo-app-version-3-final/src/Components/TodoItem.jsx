1dc77cb7895ad6d9b4241db3fe30b73b5af64a8d
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-16 22:41:38 +0530

    using useContext(TodoContext) we print the list of TodoItems without passing as a prop

--- 3-todo-app-version-3-final/src/Components/TodoItem.jsx
+++ 3-todo-app-version-3-final/src/Components/TodoItem.jsx
@@ -1,16 +1,15 @@
+import { useContext } from "react";
+import { TodoItemContext } from "../Store/TodoItem-Context";
 import TodoItem1 from "./TodoItem1";
 import style from "./TodoItemsContainer.module.css";
 
-const TodoItem = ({ todoItem, handleDeleteTodo }) => {
+const TodoItem = () => {
+  const { todoItems } = useContext(TodoItemContext);
+
   return (
     <div className={style.container}>
-      {todoItem.map((item) => (
-        <TodoItem1
-          key={item.name}
-          todoName={item.name}
-          date={item.dueDate}
-          handleDeleteTodo={handleDeleteTodo}
-        />
+      {todoItems.map((item) => (
+        <TodoItem1 key={item.name} todoName={item.name} date={item.dueDate} />
       ))}
     </div>
   );
