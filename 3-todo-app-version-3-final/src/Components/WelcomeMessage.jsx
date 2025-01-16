95395890801784488a0e7c075d15811add92e3dc
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-16 22:44:30 +0530

    Removed prop and using useContext passed TodoItems to WelcomeMessage.jsx

--- 3-todo-app-version-3-final/src/Components/WelcomeMessage.jsx
+++ 3-todo-app-version-3-final/src/Components/WelcomeMessage.jsx
@@ -1,4 +1,9 @@
-const WelcomeMessage = ({ todoItem }) => {
-  return todoItem.length === 0 && <p>Enjoy your day!</p>;
+import { useContext } from "react";
+import { TodoItemContext } from "../Store/TodoItem-Context";
+
+const WelcomeMessage = () => {
+  const { todoItems } = useContext(TodoItemContext);
+
+  return todoItems.length === 0 && <p>Enjoy your day!</p>;
 };
 export default WelcomeMessage;
