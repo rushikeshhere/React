74481983cdcb7c9b2aa9706c8ad67158fda045b9
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-17 22:41:58 +0530

    added useState and useEffect hook to change time every seconds

--- Bharat-Clock/src/App.jsx
+++ Bharat-Clock/src/App.jsx
@@ -3,12 +3,24 @@ import Slogun from "./components/Slogun";
 import CurrentTime from "./components/CurrentTime";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(timer);
+  });
+
   return (
     <center>
       <ClockTitle />
       <Slogun />
-      <CurrentTime />
+      <CurrentTime time={currentTime.toLocaleTimeString()} />
     </center>
   );
 }
