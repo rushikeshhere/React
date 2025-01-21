ffd7a399a05a7421efffce111e2532881e8c5aef
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-21 23:13:17 +0530

    added functionality on buttons

--- 5-Social-Media-app-version1/5-social-media/src/components/SideBar.jsx
+++ 5-Social-Media-app-version1/5-social-media/src/components/SideBar.jsx
@@ -1,4 +1,4 @@
-const SideBar = () => {
+const SideBar = ({ selectedTab, setSelectedTab }) => {
   return (
     <>
       <div
@@ -10,50 +10,37 @@ const SideBar = () => {
           className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
         >
           <svg className="bi pe-none me-2" width="40" height="32">
-            <use xlink:xlinkHref="#bootstrap"></use>
+            <use xlinkHref="#bootstrap"></use>
           </svg>
           <span className="fs-4">Sidebar</span>
         </a>
         <hr />
         <ul className="nav nav-pills flex-column mb-auto">
-          <li className="nav-item">
-            <a xlinkHref="#" className="nav-link active" aria-current="page">
+          <li className="nav-item" onClick={() => setSelectedTab("Home")}>
+            <a
+              xlinkHref="#"
+              className={`nav-link text-white ${
+                selectedTab === "Home" && "active"
+              }`}
+              aria-current="page"
+            >
               <svg className="bi pe-none me-2" width="16" height="16">
-                <use xlink:xlinkHref="#home"></use>
+                <use xlinkHref="#home"></use>
               </svg>
               Home
             </a>
           </li>
-          <li>
-            <a xlinkHref="#" className="nav-link text-white">
+          <li onClick={() => setSelectedTab("create post")}>
+            <a
+              xlinkHref="#"
+              className={`nav-link text-white  ${
+                selectedTab === "create post" && "active"
+              }`}
+            >
               <svg className="bi pe-none me-2" width="16" height="16">
-                <use xlink:xlinkHref="#speedometer2"></use>
+                <use xlinkHref="#speedometer2"></use>
               </svg>
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a xlinkHref="#" className="nav-link text-white">
-              <svg className="bi pe-none me-2" width="16" height="16">
-                <use xlink:xlinkHref="#table"></use>
-              </svg>
-              Orders
-            </a>
-          </li>
-          <li>
-            <a xlinkHref="#" className="nav-link text-white">
-              <svg className="bi pe-none me-2" width="16" height="16">
-                <use xlink:xlinkHref="#grid"></use>
-              </svg>
-              Products
-            </a>
-          </li>
-          <li>
-            <a xlinkHref="#" className="nav-link text-white">
-              <svg className="bi pe-none me-2" width="16" height="16">
-                <use xlink:xlinkHref="#people-circle"></use>
-              </svg>
-              Customers
+              Create Post
             </a>
           </li>
         </ul>
