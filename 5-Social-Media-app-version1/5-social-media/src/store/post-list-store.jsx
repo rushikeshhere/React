976e3332105480d65e8db63bb63aaa302ae39480
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-22 22:48:00 +0530

    added functionality to deletePost using postId

--- 5-Social-Media-app-version1/5-social-media/src/store/post-list-store.jsx
+++ 5-Social-Media-app-version1/5-social-media/src/store/post-list-store.jsx
@@ -7,6 +7,9 @@ export const PostList = createContext({
 });
 
 const postListReducer = (currentPostList, action) => {
+  if (action.type === "DELETE_POST") {
+    return currentPostList.filter((post) => post.id !== action.payload);
+  }
   return currentPostList;
 };
 
@@ -18,7 +21,12 @@ const PostListProvider = ({ children }) => {
 
   const addPost = () => {};
 
-  const deletePost = () => {};
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: postId,
+    });
+  };
 
   return (
     <PostList.Provider
@@ -37,7 +45,8 @@ const DEFAULT_POST_LIST = [
   {
     id: "1",
     title: "My first post",
-    content: "This is my first post",
+    content:
+      "In a React app, you can pass inline styles using the style attribute, which takes a JavaScript object. Here's how you can do it:",
     author: "John Doe",
     user_id: "JohnDoe1",
     date: "2023-01-01",
@@ -46,7 +55,8 @@ const DEFAULT_POST_LIST = [
   {
     id: "2",
     title: "i am going to pune ",
-    content: "hi guys i am feeling very good because i am going to pune",
+    content:
+      "hi guys i am feeling very good because i am going to pune,While inline styles are convenient for small or dynamic changes, it's better to use CSS classes or styled-components for more complex styling to keep the code clean and maintainable.",
     author: "Rushikesh here",
     user_id: "RushikeshH@1",
     date: "2025-01-01",
