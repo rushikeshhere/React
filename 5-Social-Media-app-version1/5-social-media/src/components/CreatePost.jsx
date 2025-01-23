3f577e9aced2e7faa89f1983ceb220ea0b2c7799
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-23 23:08:02 +0530

    got data using useRef

--- 5-Social-Media-app-version1/5-social-media/src/components/CreatePost.jsx
+++ 5-Social-Media-app-version1/5-social-media/src/components/CreatePost.jsx
@@ -1,42 +1,107 @@
+import { useContext, useRef } from "react";
+import { PostList } from "../store/post-list-store";
+
 const CreatePost = () => {
+  const authorElement = useRef();
+  const user_id_Element = useRef();
+  const titleElement = useRef();
+  const contentElement = useRef();
+  const dateElement = useRef();
+  const tagsElement = useRef();
+
+  const { addPost } = useContext(PostList);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const author = authorElement.current.value;
+    const user_id = user_id_Element.current.value;
+    const title = titleElement.current.value;
+    const content = contentElement.current.value;
+    const date = dateElement.current.value;
+    const tags = tagsElement.current.value.split(" ");
+
+    addPost(author, user_id, title, content, date, tags);
+
+    authorElement.current.value = "";
+    user_id_Element.current.value = "";
+    titleElement.current.value = "";
+    contentElement.current.value = "";
+    dateElement.current.value = "";
+    tagsElement.current.value = "";
+  };
+
   return (
-    <form className="form-container">
+    <form className="form-container" onSubmit={handleSubmit}>
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
+        <label htmlFor="author" className="form-label">
+          Author Name
         </label>
         <input
-          type="email"
+          type="text"
+          ref={authorElement}
           className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
+          id="author"
         />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
-        </div>
       </div>
       <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          Password
+        <label htmlFor="user_id" className="form-label">
+          User Id
         </label>
         <input
-          type="password"
+          type="text"
+          ref={user_id_Element}
           className="form-control"
-          id="exampleInputPassword1"
+          id="user_id"
         />
       </div>
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" htmlFor="exampleCheck1">
-          Check me out
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Content Title
         </label>
+        <input
+          type="text"
+          ref={titleElement}
+          className="form-control"
+          id="title"
+        />
       </div>
+      <div className="mb-3">
+        <label htmlFor="content" className="form-label">
+          Add more about content
+        </label>
+        <textarea
+          rows={5}
+          ref={contentElement}
+          className="form-control"
+          id="user_id"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="date" className="form-label">
+          Date
+        </label>
+        <input
+          type="date"
+          ref={dateElement}
+          className="form-control"
+          id="date"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Use Tags for better search
+        </label>
+        <input
+          type="text"
+          ref={tagsElement}
+          className="form-control"
+          id="tags"
+          placeholder="add tags using spaces"
+        />
+      </div>
+
       <button type="submit" className="btn btn-primary">
-        Submit
+        Create Post
       </button>
     </form>
   );
