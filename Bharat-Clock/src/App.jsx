3e1f5658091ea26448289da2e962c60e126b4592
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-28 23:16:49 +0530

    added WelcomeMessage.jsx component and added some logic to show time with title and slogun

--- Bharat-Clock/src/App.jsx
+++ Bharat-Clock/src/App.jsx
@@ -4,8 +4,10 @@ import CurrentTime from "./components/CurrentTime";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date());
+  const [openTimePage, setOpenTimePage] = useState(false);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -14,13 +16,25 @@ function App() {
 
     // Cleanup the interval on component unmount
     return () => clearInterval(timer);
-  });
+  }, []);
+
+  const handleToOpenTimePage = () => {
+    console.log(`value of openTimePage: ${openTimePage}`);
+    setOpenTimePage(true);
+  };
 
   return (
     <center>
-      <ClockTitle />
-      <Slogun />
-      <CurrentTime time={currentTime.toLocaleTimeString()} />
+      {!openTimePage && (
+        <WelcomeMessage onClickOpenTimePage={handleToOpenTimePage} />
+      )}
+
+      {openTimePage && <ClockTitle />}
+      {openTimePage && <Slogun />}
+      {openTimePage && <CurrentTime time={currentTime.toLocaleTimeString()} />}
+      {openTimePage && (
+        <button onClick={() => setOpenTimePage(false)}>Close Time Page</button>
+      )}
     </center>
   );
 }
