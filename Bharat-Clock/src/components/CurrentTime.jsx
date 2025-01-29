df4a29f1604a0e565c5fdc0e304e7fed31000dc9
Rushikesh Here <rushikeshhere@gmail.com>
2025-01-29 22:49:08 +0530

    as i am showing time! thought that why i am not added date as well so added date also to show current Date and Time

--- Bharat-Clock/src/components/CurrentTime.jsx
+++ Bharat-Clock/src/components/CurrentTime.jsx
@@ -1,7 +1,9 @@
 import styles from "./CurrentTime.module.css";
-function CurrentTime({ time }) {
+function CurrentTime({ time, date }) {
   return (
-    <p className={`lead ${styles.mine}`}>This is the current time : {time}</p>
+    <p className={`lead ${styles.mine}`}>
+      This is the current time :{date} -{""} {time}
+    </p>
   );
 }
 export default CurrentTime;
